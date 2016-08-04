e95b82e7ed68458967adaff2f48598e7578168e8
Nikita Zorin <nikita.zorin@convergent-usa.com>
2016-08-04 09:27:44 +0300

    modify struct

---
+++ src/content/MainContent.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spiker
+ * Date: 01.08.16
+ * Time: 21:48
+ */
+
+namespace speedy\content;
+
+class MainContent {
+    public function run()
+    {
+        echo 'Test any platform';
+    }
+}
---
+++ src/phpFunc/SizeofVsCount.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spiker
+ * Date: 01.08.16
+ * Time: 21:41
+ */
+namespace speedy\phpFunc;
+
+use speedy\helpers\ArrayHelper;
+
+class SizeofVsCount {
+
+    public function getTime(){
+        return microtime(true);
+    }
+
+    public function run()
+    {
+        ini_set('memory_limit', 512000000);
+
+        $params = [/*10, 100, 1000, 10000, 100000, 500000, 800000,*/ 1000000];
+        foreach($params as $size){
+            print('SIZE: '.$size.'<br/>');
+            $array = ArrayHelper::getRndArray($size,10);
+
+            flush();
+            $start_time = $this->gettime();
+            sizeof($array);
+            $stop_time = $this->gettime();
+            $time = bcsub($stop_time,$start_time,6);
+            print("Sizeof: ".$time."<br/>");
+
+            flush();
+            $start_time = $this->gettime();
+            count($array);
+            $stop_time = $this->gettime();
+            $time = bcsub($stop_time,$start_time,6);
+            print("Count: ".$time."<br/>");
+
+
+            print("---------------------<br/>");
+            unset($array);
+            flush();
+        }
+    }
+}
