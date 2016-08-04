d071d04e2fcb27f53114edbc509a316999200963
Nikita Zorin <nikita.zorin@convergent-usa.com>
2016-08-04 09:11:07 +0300

    modify struct

--- src/phpFunc/IncPrefVsPos.php
+++ src/phpFunc/IncPrefVsPos.php
@@ -7,9 +7,12 @@
  */
 namespace speedy\phpFunc;
 
-use speedy\helpers\ArrayHelper;
+use Ghunti\HighchartsPHP\Highchart;
 
 class IncPrefVsPos {
+
+    public $valueTest = [/*10, 100, 1000, 10000, 100000, 500000, 100000,*/ 1000000];
+    public $qntTest = 1;
 
     public function getTime(){
         return microtime(true);
@@ -18,23 +21,44 @@ class IncPrefVsPos {
     public function run()
     {
         ini_set('memory_limit', 512000000);
-
-        $params = [/*10, 100, 1000, 10000, 100000, 500000,*/ 100000, 1000000];
-
-        foreach($params as $size) {
-            print('SIZE: ' . $size . '<br/>');
-
-            $time1 = $this->testPost($size);
-            $time2 = $this->testPref($size);
-            print(($time1 < $time2?'*':'').'Post: '.$time1.'<br/>');
-            print(($time2 < $time1?'*':'').'Pref: '.$time2.'<br/>');
-
-            $time2 = $this->testPref($size);
-            $time1 = $this->testPost($size);
-            print(($time2 < $time1?'*':'').'Pref: '.$time2.'<br/>');
-            print(($time1 < $time2?'*':'').'Post: '.$time1.'<br/>');
-            print('-----------------------<br/>');
+        foreach($this->valueTest as $size) {
+            print('SIZE: '.$size.'<br/>');
+            $qnt = 1;
+            while($qnt <= $this->qntTest){
+                $this->itemTest($size);
+                $qnt++;
+            }
         }
+        $this->getGraph();
+    }
+
+    public function getGraph()
+    {
+        $chart = new Highchart(Highchart::HIGHSTOCK);
+        $chart->title->text = 'Monthly Average Temperature';
+        $chart->title->x = -20;
+        $chart->series[0]->name = 'Tokyo';
+        $chart->series[0]->data = array(7.0, 6.9, 9.5);
+
+        $chart->printScripts();
+        $chart->renderTo = "chart";
+        echo '<div id="chart"></div>';
+        echo '<script>'.$chart->render("chart").'</script>';
+
+    }
+
+    public function itemTest($size)
+    {
+        $time1 = $this->testPost($size);
+        $time2 = $this->testPref($size);
+        print(($time1 < $time2?'*':'').'Post: '.$time1.'<br/>');
+        print(($time2 < $time1?'*':'').'Pref: '.$time2.'<br/>');
+
+        $time2 = $this->testPref($size);
+        $time1 = $this->testPost($size);
+        print(($time2 < $time1?'*':'').'Pref: '.$time2.'<br/>');
+        print(($time1 < $time2?'*':'').'Post: '.$time1.'<br/>');
+        print('----------------------- <br/>');
     }
 
     public function testPref($size)
